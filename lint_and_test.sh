cb56ff231bf6796f4a879d1819b68eff0df9d85e
KanHar <knaan.harpaz@gmail.com>
2023-08-11 18:33:12 +0300

    WIP

--- lint_and_test.sh
+++ lint_and_test.sh
@@ -2,4 +2,5 @@ python -m black deepdream_vae
 python -m isort deepdream_vae --profile black
 python -m mypy --strict deepdream_vae
 python -m flake8 deepdream_vae
-python -m pytest tests
+python -m pip install -e .
+python -m pytest deepdream_vae/tests
